7fd037e1f0387c581baef454c7479d2b461bf8a6
MicahJank <MicahJank@users.noreply.github.com>
2020-06-03 22:31:09 -0500

    finished mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,40 +11,20 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-    midpoint = int((len(arr) - 1) / 2)
-    # sliced_arr = arr.copy()
-    # current_item = sliced_arr[midpoint]
-    # i = 0
+    # midpoint = int((len(arr) - 1) / 2)
+    start = 0
+    end = len(arr) - 1
+    mid = int((start + end) / 2)
 
-    print("array", arr)
-    while arr[midpoint] != target:
-        if target > arr[midpoint]:
-            end = len(arr) - 1
-            midpoint = int((len(arr[midpoint:end]) - 1) / 2)
-            print("midpoint greater", midpoint)
-        elif target < arr[midpoint]:
-            end = midpoint
-            midpoint = int((len(arr[:end]) - 1) / 2)
-            print("midpoint lesser", midpoint)
-
-    else:
-        print(midpoint)
-        return midpoint
-            
-    # print(sliced_arr[midpoint])
-    # while current_item != target:
-    #     if target > current_item:
-    #         sliced_arr = sliced_arr[midpoint:len(sliced_arr)-1]
-    #         print("greater than", sliced_arr)
-    #     elif target < current_item:
-    #         sliced_arr = sliced_arr[0:midpoint]
-    #         print("less than", sliced_arr)
-
-    #     midpoint = int((len(sliced_arr)-1) / 2)
-    #     current_item = sliced_arr[midpoint]
-    #     print("current item end", current_item)
-    #     print("midpoint end", midpoint)
-    # else:
-    #     return midpoint
+    if len(arr):        
+        while arr[mid] != target:
+            print(start, mid, end)
+            if arr[mid] > target:
+                end = mid
+            elif arr[mid] < target:
+                start = mid
+            mid = int((start + end) / 2) 
+        else:
+            return mid
 
     return -1  # not found
